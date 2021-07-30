6ac1df8e1c6fe3cbb1e2abdcd5d17d88d6cd34ae
Che-Ming Chang <43882897+m3gofriends@users.noreply.github.com>
2021-07-30 20:55:01 +0800

    Initial commit

--- Rock Paper Scissors/RPS.py
+++ Rock Paper Scissors/RPS.py
@@ -4,7 +4,7 @@ my_history = []
 init_play = prev_play = 'S'
 opponent_list = [False, False, False, False]
 ideal_response = {'P' : 'R', 'R' : 'S', 'S' : 'P'}
-opponent_quincy_counter = - 1
+opponent_quincy_counter = -1
 play_order = [{
     "RR" : 0, 
     "RP" : 0, 
